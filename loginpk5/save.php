d92829b771139e5f3ef36fec74b0f83f5f52b89a
AfifOsama <30397505+AfifOsama@users.noreply.github.com>
2017-10-16 09:30:01 +0700

    Add files via upload

---
+++ loginpk5/save.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<p align="center"><font size="5px"><b>INFO LOGIN ANDA</b></font></p>
+<p align="center"><table>
+<tr><td>Email</td><td>:</td><td><?php echo $_POST["Email"];?></td></tr>
+<tr><td>Fullname</td><td>:</td><td><?php echo $_POST["Nama"];?></td></tr>
+<tr><td>Username</td><td>:</td><td><?php echo $_POST["Username"];?></td></tr>
+<tr><td>Password</td><td>:</td><td><?php echo $_POST["Password"];?></td></tr>
+</table></p>
+</body>
+</html>
